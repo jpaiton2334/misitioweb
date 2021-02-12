bd8f525c689df82e00ce5e0c3d6cc4fdb95ed167
jotap <jpabloperaltacasanova@gmail.com>
2021-02-12 20:48:24 -0300

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mypage
+mis estadisticas
+Este es un proyecto donde describo las tecnologias que manejo 
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(".evento").on("click",function(){
+    $(".menu-social").toggleClass("menu-social2");
+
+})
